8294a83db5c7c0d82327f53f36912c5f1ab9ebeb
Your Name <you@example.com>
2020-12-22 19:09:45 -0500

    Cosmetics

--- Web-Scraping-Challenge/scrape_mars.py
+++ Web-Scraping-Challenge/scrape_mars.py
@@ -1,7 +1,3 @@
-#################################################
-# Jupyter Notebook Conversion to Python Script
-#################################################
-
 # Dependencies and Setup
 from bs4 import BeautifulSoup
 from splinter import Browser
@@ -9,32 +5,19 @@ import pandas as pd
 import datetime as dt
 from webdriver_manager.chrome import ChromeDriverManager
 
-#################################################
-# Mac
-#################################################
-# Set Executable Path & Initialize Chrome Browser
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-#################################################
-# NASA Mars News
-#################################################
-# NASA Mars News Site Web Scraper
 def mars_news(browser):
     # Visit the NASA Mars News Site
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
 
-    # Get First List Item & Wait Half a Second If Not Immediately Present
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
     
     html = browser.html
     news_soup = BeautifulSoup(html, "html.parser")
 
-    # Parse Results HTML with BeautifulSoup
-    # Find Everything Inside:
-    #   <ul class="item_list">
-    #     <li class="slide">
     try:
         slide_element = news_soup.select_one("ul.item_list li.slide")
         slide_element.find("div", class_="content_title")
@@ -48,27 +31,19 @@ def mars_news(browser):
         return None, None
     return news_title, news_paragraph
 
-
-#################################################
-# JPL Mars Space Images - Featured Image
-#################################################
 # NASA JPL (Jet Propulsion Laboratory) Site Web Scraper
 def featured_image(browser):
     # Visit the NASA JPL (Jet Propulsion Laboratory) Site
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url)
 
-    # Ask Splinter to Go to Site and Click Button with Class Name full_image
-    # <button class="full_image">Full Image</button>
     full_image_button = browser.find_by_id("full_image")
     full_image_button.click()
 
-    # Find "More Info" Button and Click It
     browser.is_element_present_by_text("more info", wait_time=1)
     more_info_element = browser.find_link_by_partial_text("more info")
     more_info_element.click()
 
-    # Parse Results HTML with BeautifulSoup
     html = browser.html
     image_soup = BeautifulSoup(html, "html.parser")
 
@@ -77,13 +52,10 @@ def featured_image(browser):
         img_url = img.get("src")
     except AttributeError:
         return None 
-   # Use Base URL to Create Absolute URL
+
     img_url = f"https://www.jpl.nasa.gov{img_url}"
     return img_url
 
-#################################################
-# Mars Facts
-#################################################
 # Mars Facts Web Scraper
 def mars_facts():
     # Visit the Mars Facts Site Using Pandas to Read
@@ -96,10 +68,6 @@ def mars_facts():
 
     return df.to_html(classes="table table-striped")
 
-
-#################################################
-# Mars Hemispheres
-#################################################
 # Mars Hemispheres Web Scraper
 def hemisphere(browser):
     # Visit the USGS Astrogeology Science Center Site
@@ -108,25 +76,19 @@ def hemisphere(browser):
 
     hemisphere_image_urls = []
 
-    # Get a List of All the Hemisphere
     links = browser.find_by_css("a.product-item h3")
     for item in range(len(links)):
         hemisphere = {}
-        
-        # Find Element on Each Loop to Avoid a Stale Element Exception
+
         browser.find_by_css("a.product-item h3")[item].click()
-        
-        # Find Sample Image Anchor Tag & Extract <href>
+
         sample_element = browser.find_link_by_text("Sample").first
         hemisphere["img_url"] = sample_element["href"]
-        
-        # Get Hemisphere Title
+
         hemisphere["title"] = browser.find_by_css("h2.title").text
-        
-        # Append Hemisphere Object to List
+
         hemisphere_image_urls.append(hemisphere)
         
-        # Navigate Backwards
         browser.back()
     return hemisphere_image_urls
 
@@ -145,10 +107,6 @@ def scrape_hemisphere(html_text):
     }
     return hemisphere
 
-
-#################################################
-# Main Web Scraping Bot
-#################################################
 def scrape_all():
     news_title, news_paragraph = mars_news(browser)
     img_url = featured_image(browser)
